fb920c81a44b3e53ad2f2097ef9549b6631a2a45
Eric DeCourcy <eric@openzeppelin.com>
2026-04-30 22:15:45 -0600

    add basic functionality to connect to 0g

--- app.js
+++ app.js
@@ -1,14 +1,62 @@
 async function connect() {
+if (!window.ethereum) {
+    alert("Install MetaMask");
+    return;
+}
+
+const provider = new ethers.BrowserProvider(window.ethereum);
+await provider.send("eth_requestAccounts", []);
+
+const signer = await provider.getSigner();
+const address = await signer.getAddress();
+
+document.getElementById("address").innerText = address;
+}
+
+async function getChainId() {
+if (!window.ethereum) {
+    alert("Install MetaMask");
+    return;
+}
+
+const provider = new ethers.BrowserProvider(window.ethereum);
+const network = await provider.getNetwork();
+
+document.getElementById("chain").innerText = network.chainId.toString();
+}
+
+async function switchChain() {
     if (!window.ethereum) {
       alert("Install MetaMask");
       return;
     }
   
-    const provider = new ethers.BrowserProvider(window.ethereum);
-    await provider.send("eth_requestAccounts", []);
+    const chainId = "0x4115";
   
-    const signer = await provider.getSigner();
-    const address = await signer.getAddress();
-  
-    document.getElementById("address").innerText = address;
-  }
+    try {
+      await window.ethereum.request({
+        method: "wallet_switchEthereumChain",
+        params: [{ chainId }]
+      });
+    } catch (err) {
+      // If chain not added yet
+      if (err.code === 4902) {
+        await window.ethereum.request({
+          method: "wallet_addEthereumChain",
+          params: [{
+            chainId,
+            chainName: "Ethereum Mainnet",
+            rpcUrls: ["https://evmrpc.0g.ai"],
+            nativeCurrency: {
+              name: "0g",
+              symbol: "0G",
+              decimals: 18
+            },
+            blockExplorerUrls: ["explorer.0g.ai/mainnet"]
+          }]
+        });
+      } else {
+        console.error(err);
+      }
+    }
+}
